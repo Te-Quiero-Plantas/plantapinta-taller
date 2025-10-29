f4e2bcb69ba44d2281b33bca4c003d0d69dd56e8
carlosvignolosoto-01 <carlos.vignolo.soto@gmail.com>
2025-10-28 21:45:11 -0500

    Create plans.ts

---
+++ src/data/plans.ts
@@ -0,0 +1,8 @@
+import type { Plan } from '../context/CartContext'
+
+
+export const PLANS: Plan[] = [
+{ id: 'padre-hijo', title: 'Padre/Madre + 1 hijo/a', price: 99 },
+{ id: 'padre-2hijos', title: 'Padre/Madre + 2 hijos/as', price: 149 },
+{ id: 'adicional', title: 'Hijo/a adicional', price: 45 },
+]
