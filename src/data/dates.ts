689689bc59d0e92150dec9950d9c1f5e235146ea
carlosvignolosoto-01 <carlos.vignolo.soto@gmail.com>
2025-10-28 21:46:23 -0500

    Create dates.ts

---
+++ src/data/dates.ts
@@ -0,0 +1,8 @@
+import type { WorkshopDate } from '../context/CartContext'
+
+
+export const WORKSHOP_DATES: WorkshopDate[] = [
+{ id: '2025-11-22-10am', dateLabel: 'Sáb 22 Nov, 10:00', capacity: 15, enrolled: 9 },
+{ id: '2025-11-22-4pm', dateLabel: 'Sáb 22 Nov, 16:00', capacity: 15, enrolled: 14 },
+{ id: '2025-11-29-10am', dateLabel: 'Sáb 29 Nov, 10:00', capacity: 12, enrolled: 6 },
+]
