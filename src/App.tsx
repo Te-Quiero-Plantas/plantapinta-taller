52f7aa0c0b76c8ef6888c62a7955df5afaf63141
carlosvignolosoto-01 <carlos.vignolo.soto@gmail.com>
2025-10-29 22:09:21 -0500

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ import { PlansSection } from "./components/checkout/PlansSection";
 import { DatesSection } from "./components/checkout/DatesSection";
 import { CartSection } from "./components/checkout/CartSection";
 import { HeaderBar } from './components/checkout/HeaderBar';
+import { WorkshopTypeSelector } from './components/checkout/WorkshopTypeSelector';
 
 export default function App() {
   return (
@@ -13,6 +14,7 @@ export default function App() {
       <HeaderBar />
       <main className="max-w-6xl mx-auto px-4 md:px-6 lg:px-8 py-10">
         <HeroCTA />
+        <WorkshopTypeSelector />
         <IncludesSection />
         <PlansSection />
         <DatesSection />
