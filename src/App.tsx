e20c50dda424c1fdb2015e4d88c7db42f3bdd2fc
carlosvignolosoto-01 <carlos.vignolo.soto@gmail.com>
2025-10-29 22:28:30 -0500

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,28 @@
-import React from "react";
-import { CartProvider } from "./context/CartContext";
-import { HeroCTA } from "./components/checkout/HeroCTA";
-import { IncludesSection } from "./components/checkout/IncludesSection";
-import { PlansSection } from "./components/checkout/PlansSection";
-import { DatesSection } from "./components/checkout/DatesSection";
-import { CartSection } from "./components/checkout/CartSection";
-import { HeaderBar } from './components/checkout/HeaderBar';
-import { WorkshopTypeSelector } from './components/checkout/WorkshopTypeSelector';
+import React from 'react'
+import { CartProvider } from './context/CartContext'
+import { HeaderBar } from './components/checkout/HeaderBar'
+import { ErrorBoundary } from './components/checkout/ErrorBoundary'
+import { HeroCTA } from './components/checkout/HeroCTA'
+import { WorkshopTypeSelector } from './components/checkout/WorkshopTypeSelector'
+import { IncludesSection } from './components/checkout/IncludesSection'
+import { PlansSection } from './components/checkout/PlansSection'
+import { DatesSection } from './components/checkout/DatesSection'
+import { CartSection } from './components/checkout/CartSection'
 
 export default function App() {
   return (
     <CartProvider>
-      <HeaderBar />
-      <main className="max-w-6xl mx-auto px-4 md:px-6 lg:px-8 py-10">
-        <HeroCTA />
-        <WorkshopTypeSelector />
-        <IncludesSection />
-        <PlansSection />
-        <DatesSection />
-        <CartSection />
-      </main>
+      <ErrorBoundary>
+        <HeaderBar />
+        <main className="max-w-6xl mx-auto px-4 md:px-6 lg:px-8 py-10">
+          <HeroCTA />
+          <WorkshopTypeSelector />
+          <IncludesSection />
+          <PlansSection />
+          <DatesSection />
+          <CartSection />
+        </main>
+      </ErrorBoundary>
     </CartProvider>
   )
 }
